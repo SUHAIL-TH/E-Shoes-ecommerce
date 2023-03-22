a82ef7c129e8d0d7f97823a81d93a99d93fc69fc
Suhail TH <suhailth17756@gmail.com>
2023-03-22 23:27:06 +0530

    category listing commit

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@ const express=require("express");
 userRouter=express()
 const userController=require("../controllers/usercontroller")
 const verifyLogin=require("../middleware/session")
+const blockeduser=require("../middleware/blockedUser")
 
 
 
 
-userRouter.get("/",userController.home);
+userRouter.get("/",blockeduser,userController.home);
 userRouter.get("/login", userController.login)
 userRouter.get("/signup",userController.signup)
 userRouter.post("/signup",userController.postsignup)
@@ -15,5 +16,14 @@ userRouter.post("/login",userController.postlogin)
 userRouter.get("/logout",userController.logout)
 userRouter.get("/forgetpassword",userController.forgetpassword)
 userRouter.post("/forgetpassword",userController.postforgetpassword)
+userRouter.get("/resetpassword",userController.getresetpassword)
+userRouter.post("/resetpassword",userController.resetpassword)
+
+
+userRouter.get("/shop",blockeduser,verifyLogin.verifyUserLogin,userController.getshop)
+
+userRouter.get("/cart",blockeduser,verifyLogin.verifyUserLogin,userController.getcart)
+
+userRouter.get("/viewproduct/:id",userController.viewproduct)
 
 module.exports=userRouter
