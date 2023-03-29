cb55026fc59a416e26afa182b1f7f94de677a39c
Suhail TH <suhailth17756@gmail.com>
2023-03-29 22:57:02 +0530

    addto cart ajax

--- public/javascript/ajax.js
+++ public/javascript/ajax.js
@@ -6,9 +6,16 @@ function addtocart(proId){
         method:"get",
         success:(response)=>{
             if(response.status){
+                swal({
+                    title: "Product added to cart",
+                    text: "",
+                    icon: "success",
+                    button: "Ok",
+                  });
                 let count=$('#cartcount').html()
                 count=parseInt(count)+1
                 $("#cartcount").html(count)
+                
             }
 
         }
