31988f79f6039d3177e2a77c5eea8c0257489480
Suhail TH <suhailth17756@gmail.com>
2023-03-29 22:58:46 +0530

    5th commit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express=require("express");
-userRouter=express()
+const userRouter=express()
 const userController=require("../controllers/usercontroller")
+const productController=require("../controllers/productController")
+const categoryController=require("../controllers/categoryController")
 const verifyLogin=require("../middleware/session")
 const blockeduser=require("../middleware/blockedUser")
 
@@ -17,17 +19,27 @@ userRouter.get("/logout",userController.logout)
 userRouter.get("/forgetpassword",userController.forgetpassword)
 userRouter.post("/forgetpassword",userController.postforgetpassword)
 userRouter.get("/resetpassword",userController.getresetpassword)
-userRouter.post("/resetpassword",userController.resetpassword)
+userRouter.post("/resetpassword",blockeduser,userController.resetpassword)
+userRouter.get("/profile",blockeduser,verifyLogin.verifyUserLogin,userController.profile)
+userRouter.post("/postprofile",blockeduser,verifyLogin.verifyUserLogin,userController.postprofile)
 
 
 userRouter.get("/shop",blockeduser,verifyLogin.verifyUserLogin,userController.getshop)
 
-userRouter.get("/cart",blockeduser,verifyLogin.verifyUserLogin,userController.getcart)
 
-userRouter.get("/viewproduct/:id",userController.viewproduct)
 
-userRouter.get("/categoryproduct",userController.categoryproduct)
+userRouter.get("/viewproduct/:id",productController.viewproductuser)
+userRouter.post("/changeproductquantity",userController.changeproductquantity,)
+
+userRouter.get("/categoryproduct",blockeduser,categoryController.categoryproduct)
 
 userRouter.get("/addtocart/:id",verifyLogin.verifyUserLogin,userController.addtocart)
+userRouter.get("/cart",blockeduser,verifyLogin.verifyUserLogin,userController.getcart)
+userRouter.post("/removeproduct",userController.removeproduct)
+
+userRouter.get("/checkout",verifyLogin.verifyUserLogin,userController.checkout)
+userRouter.post("/addaddress",verifyLogin.verifyUserLogin,userController.addaddress)
+userRouter.get("/deleteaddress/:id",verifyLogin.verifyUserLogin,userController.deleteaddress)
+
 
 module.exports=userRouter
