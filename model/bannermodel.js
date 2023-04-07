3b7d522f17472586a134f9e2859dbe823de1e1dc
Suhail TH <suhailth17756@gmail.com>
2023-04-07 11:32:06 +0530

    banner model

---
+++ model/bannermodel.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose")
+
+const bannerSchema=new mongoose.Schema({
+    heading:{
+        type:String
+    },
+    discription:{
+        type:String
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:Boolean,
+        default:true
+    }
+})
+
+const banner=mongoose.model("banner",bannerSchema)
+module.exports=banner
