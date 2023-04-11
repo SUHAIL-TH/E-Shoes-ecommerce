2612b51677b631e2f54f4aa8249310afedcb381d
Suhail TH <suhailth17756@gmail.com>
2023-04-11 20:04:19 +0530

    app js commit

--- app.js
+++ app.js
@@ -1,53 +1,48 @@
-const express=require("express")
-const app=express()
-const session=require("express-session")
-const path=require("path")
-const adminRouter=require("./routes/admin")
-const userRouter=require("./routes/user")
-const cookiparser=require("cookie-parser")
-const morgon=require("morgan")
-const dotenv=require("dotenv")
-const dbconnect=require("./config/connection")
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const path = require("path");
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+const cookiparser = require("cookie-parser");
+const morgon = require("morgan");
+const dotenv = require("dotenv");
+const dbconnect = require("./config/connection");
 
+dotenv.config();
+dbconnect.dbconnect();
 
-dotenv.config()
-dbconnect.dbconnect()
- 
-app.set("views")
-app.set("view engine",'ejs')
-app.set(cookiparser())
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.set("views");
+app.set("view engine", "ejs");
+app.set(cookiparser());
+app.use(express.static(path.join(__dirname, "public")));
 
 //morgon setup
-app.use(morgon('dev'))
-app.use(express.json())
-app.use(express.urlencoded ({extended:true}))
+app.use(morgon("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //session management
-app.use(session({
+app.use(
+  session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:false,
+    saveUninitialized: false,
     cookie: { maxAge: 6000000 },
-    resave: false 
-}))
+    resave: false,
+  })
+);
 
 //remove cache
 app.use((req, res, next) => {
-    res.header(
-      "Cache-Control",
-      "no-cache,  no-store, must-revalidate"
-    );
-    next();
+  res.header("Cache-Control", "no-cache,  no-store, must-revalidate");
+  next();
 });
-app.use("/admin",adminRouter)
-app.use("/",userRouter)
+app.use("/admin", adminRouter);
+app.use("/", userRouter);
 
-app.use((req,res)=>{
-    res.status(404).render("404")
-})
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server started listening ato port");
-})
-
+app.listen(process.env.PORT, () => {
+  console.log("Server started listening ato port");
+});
